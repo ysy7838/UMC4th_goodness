466f00e43ba34d2e064658345b73b8f26771081b
minsub <chrkb1569@naver.com>
2023-07-06 15:11:07 +0900

    feat : Domain의 생성 및 수정 일자를 파악하기 위한 어노테이션 추가

--- precending/src/main/java/umc/precending/PrecendingApplication.java
+++ precending/src/main/java/umc/precending/PrecendingApplication.java
@@ -2,8 +2,10 @@ package umc.precending;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PrecendingApplication {
 
 	public static void main(String[] args) {

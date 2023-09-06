cbabf6ef31071db3adeca26ee22df6d59f668155
minsub <chrkb1569@naver.com>
2023-09-06 19:22:50 +0900

    feat : PostRepository에 인증 여부에 따라서 게시글을 조회하는 기능 추가

--- precending/src/main/java/umc/precending/repository/post/PostRepository.java
+++ precending/src/main/java/umc/precending/repository/post/PostRepository.java
@@ -6,6 +6,7 @@ import umc.precending.domain.Post.Post;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByWriterAndVerifiable(String writer, boolean verifiable);
     List<Post> findAllByWriterAndYearAndMonth(String writer, int year, int month);
     List<Post> findPostsByVerifiableAndIsVerified(boolean verifiable, boolean ifVerified);
 }

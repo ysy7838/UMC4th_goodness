b35638e13cdc0867678ffd5bdc9791725a2d94de
minsub <chrkb1569@naver.com>
2023-09-06 19:24:22 +0900

    feat : MemberController에 인증 가능한 게시글 성향 조회, 인증 불가능한 게시글 성향 조회 기능 추가

--- precending/src/main/java/umc/precending/controller/member/MemberController.java
+++ precending/src/main/java/umc/precending/controller/member/MemberController.java
@@ -6,12 +6,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import umc.precending.domain.member.Member;
 import umc.precending.exception.member.MemberNotFoundException;
 import umc.precending.repository.member.MemberRepository;
+import umc.precending.response.Response;
 import umc.precending.service.member.MemberService;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -28,6 +35,22 @@ public class MemberController {
     public void saveImage(@ModelAttribute MultipartFile file) {
         Member member = getMember();
         memberService.saveImage(file, member);
+    }
+
+    @GetMapping("/member/valid")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "게시글 조회 - 인증 가능", notes = "사용자가 작성한 인증 가능한 게시글들의 현황을 조회하는 로직")
+    public Response getValidPostStatus() {
+        Member member = getMember();
+        return Response.success(memberService.getValidPostStatus(member));
+    }
+
+    @GetMapping("/member/normal")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "게시글 조회 - 인증 불가능", notes = "사용자가 작성한 인증 불가능한 게시글들의 현황을 조회하는 로직")
+    public Response getNormalPostStatus() {
+        Member member = getMember();
+        return Response.success(memberService.getNormalPostStatus(member));
     }
 
     // 현재 로그인하고 있는 사용자의 정보를 통해 정보 반환
